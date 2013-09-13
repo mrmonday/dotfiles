f22ff3c043836b762320ab9ef9a300b61a45148b
Robert Clipsham <robert@octarineparrot.com>
2013-09-13 15:38:52 +0100

    Initial commit of config files.
    I have no idea why this wasn't done sooner.

---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+###
+# Copy over config files
+#
+# Authors:
+#   Robert Clipsham <robert@octarineparrot.com>
+###
+
+# AA of config files
+# Example:
+#    config_files[foobar]="cf1 cf2"
+# Result:
+#    ln -s foobar/cf1 ~/.cf1
+#    ln -s foobar/cf2 ~/.cf2
+declare -A config_files
+
+config_files[X11]="Xresources"
+config_files[git]="gitconfig"
+config_files[vim]="vimrc"
+
+cd $(dirname $0)
+
+##
+# Should config files for $1 be installed?
+#
+# Params:
+#    $1 = Directory name/application name to prompt the user for
+# Returns:
+#    1 if config files should be copied, 0 otherwise
+##
+should_install() {
+    echo -n "Install config files for $1? [Y/n]: "
+    read yn
+    case $yn in
+        [Nn]*)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+##
+# Install configuration files for $1
+#
+# Params:
+#    $1 = Directory/application name to install config files for
+##
+install_config() {
+    for file in ${config_files[$1]}; do
+        ln -s `pwd`/$1/$file ~/.$file
+    done
+}
+
+##
+# Prompt the user to install each config file
+##
+for app in "${!config_files[@]}"; do
+    should_install $app
+    if (( $? == 1 )); then
+        install_config $app
+    fi
+done
